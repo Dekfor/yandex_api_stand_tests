4ff20606c141a32e6902cafeedbd5fc5f525a19c
Dekfor <Dekfor@yandex.ru>
2023-09-27 21:48:25 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://e3be6f65-ae01-4b75-b59a-4f6f68fedaa0.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
